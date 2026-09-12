58c0e7584a9f6c20bdb92861e23a3708e08a4245
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2026-09-13 00:54:09 +0700

    fix(deploy): grant GitHub Actions EKS access

--- terraform/singapore-dev/main.tf
+++ terraform/singapore-dev/main.tf
@@ -101,6 +101,28 @@ resource "aws_eks_access_policy_association" "bastion_admin" {
   }
 }
 
+data "aws_iam_role" "github_actions" {
+  name = "${var.project_name}-github-actions-role"
+}
+
+resource "aws_eks_access_entry" "github_actions" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = data.aws_iam_role.github_actions.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "github_actions_admin" {
+  cluster_name  = module.eks.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = data.aws_iam_role.github_actions.arn
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.github_actions]
+}
+
 data "aws_eks_cluster" "this" {
   name       = module.eks.cluster_name
   depends_on = [module.eks]
